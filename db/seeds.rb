bb9d6c4e1b28b3655c50371d6cfdda13a1146e4a
Marc Loher <marc.loher@interfaceurope.eu>
2017-02-22 18:12:26 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Restaurant.create(name: Faker::Company.name, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, address: Faker::Address.street_address + Faker::Address.city, phone_number: Faker::PhoneNumber.phone_number)
+end
+
+Restaurant.destroy_all
+
